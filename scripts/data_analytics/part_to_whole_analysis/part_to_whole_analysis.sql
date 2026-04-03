c75a8d09777546ac0394ba0fad2faff5c8462fc3
derekvarghese <89374105+derekvarghese@users.noreply.github.com>
2026-04-03 23:19:58 +0530

    Create part_to_whole_analysis.sql

---
+++ scripts/data_analytics/part_to_whole_analysis/part_to_whole_analysis.sql
@@ -0,0 +1,29 @@
+-- Part-to-Whole Analysis
+-- Which categories contribute the most to overall sales
+
+WITH category_sales AS (
+    SELECT
+        p.category,
+        SUM(f.sales_amount) AS total_sales
+    FROM gold.fact_sales f
+    LEFT JOIN gold.dim_products p
+        ON f.product_key = p.product_key
+    GROUP BY 
+        p.category
+)
+
+SELECT
+    *,
+    SUM(total_sales) OVER () AS overall_sales,
+
+    CONCAT(
+        ROUND(
+            (CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100,
+            2
+        ),
+        '%'
+    ) AS total_sales_percentage
+
+FROM category_sales
+ORDER BY 
+    total_sales DESC;
